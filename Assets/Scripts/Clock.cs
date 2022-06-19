d6c30300e8041b998da13842938cb62a8494da24
Ran Zonis <zonisr@gmail.com>
2022-06-19 19:53:13 +0300

    the clock ticks

--- Assets/Scripts/Clock.cs
+++ Assets/Scripts/Clock.cs
@@ -10,10 +10,9 @@ public class Clock : MonoBehaviour
     [SerializeField] Transform hand2;
 
     [SerializeField] float speed = 0.01f;
-    [SerializeField] float timeCount = 0.0f;
 
-    private float angle1;
-    private float angle2;
+    [SerializeField] private float angle1;
+    [SerializeField] private float angle2;
 
 
     // Start is called before the first frame update
@@ -23,17 +22,30 @@ public class Clock : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         DateTime time = System.DateTime.Now;
 
-        angle1 = time.Hour * 360 / 12;
-        angle2 = time.Minute * 360 / 60;
+        angle1 = -time.Minute * 360 / 60;
+        angle2 = -time.Second * 360 / 60;
 
-        hand1.rotation = Quaternion.Lerp(hand1.rotation, Quaternion.Euler(0, 0, angle1), timeCount * speed);
-        hand2.rotation = Quaternion.Lerp(hand1.rotation, Quaternion.Euler(0, 0, angle2), timeCount * speed);
+        hand1.rotation = Quaternion.Lerp(hand1.rotation, Quaternion.Euler(0, 0, angle1), speed);
+        hand2.rotation = Quaternion.Lerp(hand2.rotation, Quaternion.Euler(0, 0, angle2), speed);
 
+        //hand1.rotation = Quaternion.Lerp(hand1.rotation, Quaternion.Euler(0, 0, angle1), timeCount * speed);
+        //hand2.rotation = Quaternion.Lerp(hand2.rotation, Quaternion.Euler(0, 0, angle2), timeCount * speed);
 
+        //angle1 = -time.Hour * 360 / 12;
+        //angle2 = -time.Minute * 360 / 60;
+
+        //hand1.rotation = Quaternion.Lerp(hand1.rotation, Quaternion.Euler(0, 0, angle1), timeCount * speed);
+        //hand2.rotation = Quaternion.Lerp(hand1.rotation, Quaternion.Euler(0, 0, angle2), timeCount * speed);
+
+        //angle1 = -time.Second * 360 / 60;
+        //hand1.rotation = Quaternion.Lerp(hand1.rotation, Quaternion.Euler(0, 0, angle1), timeCount * speed);
+
+        //Debug.Log("Seconds: " + time.Second.ToString());
+        //Debug.Log("Angle: " + angle1);
 
     }
 }
